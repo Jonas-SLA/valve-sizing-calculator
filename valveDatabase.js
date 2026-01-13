b583dbc6f07686378baeaefc58f8dc1e8cab9768
Jonas-SLA <jonaslicassales@gmail.com>
2026-01-13 20:58:40 -0300

    Update valveDatabase.js

--- valveDatabase.js
+++ valveDatabase.js
@@ -7,7 +7,10 @@ const valveDatabase = [
         size: 4,
         ratedCv: 150,
         Fl: 0.90,
-        Fd: 0.46
+        Fd: 0.46,
+        characteristic: "Equal Percentage",
+        pressureClasses: [150, 300, 600, 900],
+        cvCurve: [8, 14, 23, 35, 52, 72, 95, 120, 138, 150]
     },
     {
         manufacturer: "Masoneilan",
@@ -16,7 +19,9 @@ const valveDatabase = [
         size: 4,
         ratedCv: 135,
         Fl: 0.89,
-        Fd: 0.46
+        Fd: 0.46,
+        pressureClasses: [150, 300, 600, 900],
+        cvCurve: [8, 14, 23, 35, 52, 72, 95, 110, 120, 135]
     },
     { // A smaller valve (reduced port)
         manufacturer: "Masoneilan",
@@ -25,7 +30,9 @@ const valveDatabase = [
         size: 3,
         ratedCv: 90,
         Fl: 0.92,
-        Fd: 0.47
+        Fd: 0.47,
+        pressureClasses: [300, 600, 900],
+        cvCurve: [8, 14, 23, 35, 52, 62, 75, 80, 85, 90]
     },
     {
         manufacturer: "Generic",
@@ -34,7 +41,9 @@ const valveDatabase = [
         size: 4,
         ratedCv: 250,
         Fl: 0.70,
-        Fd: 0.95
+        Fd: 0.95,
+        pressureClasses: [900, 1500, 2500, 4500],
+        cvCurve: [8, 14, 23, 35, 52, 72, 95, 140, 188, 250]
     }
     // ... add all your other valves here
 ];
